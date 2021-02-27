4ebbe4759fd4007c4d5c5c3dbcb4aa7010f6d4ec
ccarr66 <connorryacarr@gmail.com>
2021-02-26 19:43:12 -0800

    INIT commit

---
+++ StarField/StarField.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace StarField
+{
+    partial class StarField
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_DragRegion = new System.Windows.Forms.Button();
+            this.btn_ResetImage = new System.Windows.Forms.Button();
+            this.btn_Play = new System.Windows.Forms.Button();
+            this.btn_Maximize = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.pctBx_Display = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBx_Display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_DragRegion
+            // 
+            this.btn_DragRegion.FlatAppearance.BorderSize = 0;
+            this.btn_DragRegion.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btn_DragRegion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btn_DragRegion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_DragRegion.Location = new System.Drawing.Point(0, 0);
+            this.btn_DragRegion.Name = "btn_DragRegion";
+            this.btn_DragRegion.Size = new System.Drawing.Size(783, 20);
+            this.btn_DragRegion.TabIndex = 0;
+            this.btn_DragRegion.TabStop = false;
+            this.btn_DragRegion.UseVisualStyleBackColor = true;
+            this.btn_DragRegion.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn_DragRegion_MouseDown);
+            this.btn_DragRegion.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn_DragRegion_MouseMove);
+            this.btn_DragRegion.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btn_DragRegion_MouseUp);
+            // 
+            // btn_ResetImage
+            // 
+            this.btn_ResetImage.BackgroundImage = global::StarField.Properties.Resources.reset;
+            this.btn_ResetImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_ResetImage.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_ResetImage.FlatAppearance.BorderSize = 0;
+            this.btn_ResetImage.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btn_ResetImage.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btn_ResetImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ResetImage.ForeColor = System.Drawing.Color.Gray;
+            this.btn_ResetImage.Location = new System.Drawing.Point(792, 633);
+            this.btn_ResetImage.Name = "btn_ResetImage";
+            this.btn_ResetImage.Size = new System.Drawing.Size(20, 20);
+            this.btn_ResetImage.TabIndex = 0;
+            this.btn_ResetImage.TabStop = false;
+            this.btn_ResetImage.UseVisualStyleBackColor = false;
+            this.btn_ResetImage.Click += new System.EventHandler(this.btn_ResetImage_Click);
+            // 
+            // btn_Play
+            // 
+            this.btn_Play.BackgroundImage = global::StarField.Properties.Resources.Play_Button;
+            this.btn_Play.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_Play.FlatAppearance.BorderSize = 0;
+            this.btn_Play.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btn_Play.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btn_Play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Play.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_Play.Location = new System.Drawing.Point(12, 633);
+            this.btn_Play.Name = "btn_Play";
+            this.btn_Play.Size = new System.Drawing.Size(20, 20);
+            this.btn_Play.TabIndex = 0;
+            this.btn_Play.TabStop = false;
+            this.btn_Play.UseVisualStyleBackColor = true;
+            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+            // 
+            // btn_Maximize
+            // 
+            this.btn_Maximize.BackgroundImage = global::StarField.Properties.Resources.MaxButton;
+            this.btn_Maximize.FlatAppearance.BorderSize = 0;
+            this.btn_Maximize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btn_Maximize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btn_Maximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Maximize.Location = new System.Drawing.Point(783, 0);
+            this.btn_Maximize.Name = "btn_Maximize";
+            this.btn_Maximize.Size = new System.Drawing.Size(20, 20);
+            this.btn_Maximize.TabIndex = 0;
+            this.btn_Maximize.TabStop = false;
+            this.btn_Maximize.UseVisualStyleBackColor = true;
+            this.btn_Maximize.Click += new System.EventHandler(this.btn_maximize_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackgroundImage = global::StarField.Properties.Resources.X_button;
+            this.btn_Close.FlatAppearance.BorderSize = 0;
+            this.btn_Close.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btn_Close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Location = new System.Drawing.Point(803, 0);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(20, 20);
+            this.btn_Close.TabIndex = 0;
+            this.btn_Close.TabStop = false;
+            this.btn_Close.UseVisualStyleBackColor = false;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // pctBx_Display
+            // 
+            this.pctBx_Display.BackColor = System.Drawing.Color.White;
+            this.pctBx_Display.Location = new System.Drawing.Point(12, 26);
+            this.pctBx_Display.Name = "pctBx_Display";
+            this.pctBx_Display.Size = new System.Drawing.Size(800, 600);
+            this.pctBx_Display.TabIndex = 0;
+            this.pctBx_Display.TabStop = false;
+            // 
+            // StarField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(824, 660);
+            this.Controls.Add(this.btn_ResetImage);
+            this.Controls.Add(this.btn_Play);
+            this.Controls.Add(this.btn_Maximize);
+            this.Controls.Add(this.btn_DragRegion);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.pctBx_Display);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(824, 660);
+            this.Name = "StarField";
+            this.ResizeEnd += new System.EventHandler(this.StarField_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.pctBx_Display)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctBx_Display;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_DragRegion;
+        private System.Windows.Forms.Button btn_Maximize;
+        private System.Windows.Forms.Button btn_Play;
+        private System.Windows.Forms.Button btn_ResetImage;
+    }
+}
+
---
+++ StarField/StarField.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StarField
+{
+    public partial class StarField : Form
+    {
+        const int NumStars = 600;
+        PointF CameraVelocityVec;
+        PointF CameraAccelerationVec;
+        Random CameraAdjRnd;
+        Point GlobCenter;
+
+        private bool mouseDown;
+        private Point lastLocation;
+
+        private List<Star> Stars;
+
+        private Bitmap Background;
+
+        private bool playMode;
+        private System.Windows.Forms.Timer checkTimer;
+        
+        public StarField()
+        {
+            InitializeComponent();
+
+            checkTimer = new System.Windows.Forms.Timer();
+            checkTimer.Tick += new EventHandler(continueGame);
+
+            Stars = new List<Star>((int)NumStars);
+            GlobCenter = new Point((int)(pctBx_Display.Size.Width / 2), (int)(pctBx_Display.Size.Height / 2));
+
+            Star.Center = new Point((int)(pctBx_Display.Size.Width / 2), (int)(pctBx_Display.Size.Height / 2));
+            Star.ScreenSize = pctBx_Display.Size;
+
+            //CameraAdjRnd = new Random();
+            //CameraVelocityVec = new PointF((float)(5 * (CameraAdjRnd.NextDouble() - 0.5)), (float)(5 * (CameraAdjRnd.NextDouble() - 0.5)));
+            //CameraAccelerationVec = new PointF((float)(2 * (CameraAdjRnd.NextDouble() - 0.5)), (float)(2 * (CameraAdjRnd.NextDouble() - 0.5)));
+            newStars();
+        }
+        private void generateBackgroundImage()
+        {
+            Background = new Bitmap(pctBx_Display.Size.Width, pctBx_Display.Size.Height);
+            for (int x = 0; x < Background.Width; x++)
+                for (int y = 0; y < Background.Height; y++)
+                    Background.SetPixel(x, y, Star.SkyColor);
+        }
+        private void updateDisplay()
+        {
+            int imgWidth = pctBx_Display.Width, imgHeight = pctBx_Display.Height;
+
+            if (pctBx_Display.Image != null)
+                pctBx_Display.Image.Dispose();
+
+            /*if(CameraAdjRnd.NextDouble() > 0.8)
+                CameraAccelerationVec = new PointF((float)(1 * (CameraAdjRnd.NextDouble() - 0.5)), (float)(1 * (CameraAdjRnd.NextDouble() - 0.5)));
+
+            if (CameraAdjRnd.NextDouble() > 0.8)
+                CameraVelocityVec = new PointF((float)(1 * (CameraAdjRnd.NextDouble() - 0.5)), (float)(1 * (CameraAdjRnd.NextDouble() - 0.5)));
+
+            CameraVelocityVec.X += CameraAccelerationVec.X;
+            CameraVelocityVec.Y += CameraAccelerationVec.Y;
+
+            if (Math.Pow(Star.Center.X + (int)CameraVelocityVec.X - GlobCenter.X, 2) + Math.Pow(Star.Center.Y + (int)CameraVelocityVec.Y - GlobCenter.Y, 2) < 4000)
+            {
+                Star.Center.X += (int)CameraVelocityVec.X;
+                Star.Center.Y += (int)CameraVelocityVec.Y;
+            }*/
+
+            Bitmap Frame = new Bitmap(Background);
+            foreach (var star in Stars)
+            {
+                star.Move();
+
+                star.DrawStar(ref Frame);
+            }
+
+            pctBx_Display.Image = Frame;
+            pctBx_Display.Refresh();
+        }
+
+        //Event Handlers
+        protected override void WndProc(ref Message m)
+        {
+            const int RESIZE_HANDLE_SIZE = 10;
+
+            switch (m.Msg)
+            {
+                case 0x0084/*NCHITTEST*/ :
+                    base.WndProc(ref m);
+
+                    if ((int)m.Result == 0x01/*HTCLIENT*/)
+                    {
+                        Point screenPoint = new Point(m.LParam.ToInt32());
+                        Point clientPoint = this.PointToClient(screenPoint);
+                        if (clientPoint.Y <= RESIZE_HANDLE_SIZE)
+                        {
+                            /*if (clientPoint.X <= RESIZE_HANDLE_SIZE)
+                                m.Result = (IntPtr)13;//HTTOPLEFT
+                            else if (clientPoint.X < (Size.Width - RESIZE_HANDLE_SIZE))
+                                m.Result = (IntPtr)12;//HTTOP
+                            else
+                                m.Result = (IntPtr)14;//HTTOPRIGHT*/
+                        }
+                        else if (clientPoint.Y <= (Size.Height - RESIZE_HANDLE_SIZE))
+                        {
+                            if (clientPoint.X <= RESIZE_HANDLE_SIZE)
+                                m.Result = (IntPtr)10/*HTLEFT*/ ;
+                            else if (clientPoint.X < (Size.Width - RESIZE_HANDLE_SIZE))
+                                m.Result = (IntPtr)2/*HTCAPTION*/ ;
+                            else
+                                m.Result = (IntPtr)11/*HTRIGHT*/ ;
+                        }
+                        else
+                        {
+                            if (clientPoint.X <= RESIZE_HANDLE_SIZE)
+                                m.Result = (IntPtr)16/*HTBOTTOMLEFT*/ ;
+                            else if (clientPoint.X < (Size.Width - RESIZE_HANDLE_SIZE))
+                                m.Result = (IntPtr)15/*HTBOTTOM*/ ;
+                            else
+                                m.Result = (IntPtr)17/*HTBOTTOMRIGHT*/ ;
+                        }
+                    }
+                    return;
+            }
+            base.WndProc(ref m);
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= 0x20000; // <--- use 0x20000
+                return cp;
+            }
+        }
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btn_DragRegion_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+        private void btn_DragRegion_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+        private void btn_DragRegion_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+        private void StarField_ResizeEnd(object sender, EventArgs e)
+        {
+            resizeForm();
+        }
+        private void resizeForm()
+        {
+            this.btn_Close.Location = new Point(this.Size.Width - 20, 0);
+            this.pctBx_Display.Size = new Size(this.Size.Width - pctBx_Display.Location.X - 12, this.Size.Height - pctBx_Display.Location.Y - 34);
+            this.btn_DragRegion.Size = new Size(this.Size.Width - 40, 20);
+            this.btn_Maximize.Location = new Point(this.Size.Width - 40, 0);
+
+            this.btn_Play.Location = new Point(12, this.Size.Height - 27);
+
+            this.btn_ResetImage.Location = new Point(this.Size.Width - 32, this.Size.Height - 28);
+
+            GlobCenter = new Point((int)(pctBx_Display.Size.Width / 2), (int)(pctBx_Display.Size.Height / 2));
+
+            Star.Center = new Point((int)(pctBx_Display.Size.Width / 2), (int)(pctBx_Display.Size.Height / 2));
+            Star.ScreenSize = pctBx_Display.Size;
+
+            newStars();
+        }
+        private void btn_maximize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState != FormWindowState.Maximized)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
+
+            StarField_ResizeEnd(sender, e);
+        }
+        private void btn_Play_Click(object sender, EventArgs e)
+        {
+            if (!playMode)
+            {
+                playMode = true;
+                //this.btn_Play.BackgroundImage = global::StarField.Properties.Resources.Pause;
+                setNextAlert();
+            }
+            else
+            {
+                playMode = false;
+                //this.btn_Play.BackgroundImage = global::StarField.Properties.Resources.Play_Button;
+            }
+        }
+        private void setNextAlert()
+        {
+            updateDisplay();
+
+            checkTimer.Interval = 1;
+            checkTimer.Start();
+        }
+        private void continueGame(object source, EventArgs e)
+        {
+            checkTimer.Stop();
+
+            if (playMode)
+            {
+                setNextAlert();
+            }
+
+        }
+        private void btn_ResetImage_Click(object sender, EventArgs e)
+        {
+            newStars();
+        }
+
+        private void newStars()
+        {
+            generateBackgroundImage();
+
+            Stars.RemoveRange(0, Stars.Count);
+            for (int st = 0; st < NumStars; st++)
+                Stars.Add(new Star());
+
+            updateDisplay();
+        }
+    }
+
+    public class Star
+    {
+        //private static Color[] Colors = { Color.LightBlue, Color.AliceBlue, Color.Blue, Color.BlueViolet, Color.CadetBlue, Color.CornflowerBlue, Color.DarkBlue, Color.DarkSlateBlue, Color.DeepSkyBlue, Color.DodgerBlue, Color.LightSkyBlue, Color.LightSteelBlue, Color.MediumBlue, Color.MediumSlateBlue, Color.MidnightBlue, Color.PowderBlue, Color.RoyalBlue, Color.SkyBlue, Color.SlateBlue, Color.SteelBlue };
+        private static Color[] Colors = { Color.LightBlue, Color.AntiqueWhite, Color.NavajoWhite, Color.GhostWhite, Color.WhiteSmoke, Color.LightYellow, Color.LightGoldenrodYellow, Color.LightCyan };
+        private static Random RandStar = new Random();
+        public static Size ScreenSize = new Size();
+        public static Point Center = new Point();
+        public static int MaxSpawnRadius = -1;
+        public static int MinSpawnRadius = -1;
+        public static float RadialMotion = 0.0f;
+        public static float DirectionRotation = 0.0f;
+        public static Color SkyColor = Color.Black;
+        public static bool RandomStarColor = false;
+
+        public Point Location;
+        public int StarSize;
+        public Color StarColor;
+
+        public PointF Direction;
+        public float Length;
+
+        public float Velocity;
+        public float Acceleration;
+        public float Jerk;
+
+        public Star()
+        {
+            Reset();
+
+            this.Acceleration = 0.001f;
+            this.Jerk = 0.0001f;
+        }
+        public void Move()
+        {
+            this.Jerk *= 1.05f;
+
+            if (this.Acceleration < 0.1f)
+                this.Acceleration += this.Jerk;
+
+            if (this.Length <= 200)
+                this.Length *= 1 + 5 * this.Acceleration;
+            this.Velocity *= 1 + this.Acceleration;
+
+            this.Location = new Point(this.Location.X + (int)(this.Velocity * Direction.X), this.Location.Y + (int)(this.Velocity * Direction.Y));
+
+            if (RadialMotion != 0)
+                this.Location = new Point((int)(Math.Cos(RadialMotion) * this.Location.X - Math.Sin(RadialMotion) * this.Location.Y), (int)(Math.Sin(RadialMotion) * this.Location.X + Math.Cos(RadialMotion) * this.Location.Y));
+
+            if (DirectionRotation != 0)
+                this.Direction = new PointF((float)(Math.Cos(DirectionRotation) * this.Direction.X - Math.Sin(DirectionRotation) * this.Direction.Y), (float)(Math.Sin(DirectionRotation) * this.Direction.X + Math.Cos(DirectionRotation) * this.Direction.Y));
+        }
+        public void DrawStar(ref Bitmap img)
+        {
+            this.StarBoundsCheck();
+
+            var CurrLoc = this.Location;
+            if (RadialMotion != 0)
+                CurrLoc = new Point(CurrLoc.X + Center.X, CurrLoc.Y + Center.Y);
+
+            var LineEnd = new Point(CurrLoc.X + (int)(this.Direction.X * this.Length), CurrLoc.Y + (int)(this.Direction.Y * this.Length));
+
+            //Brush starPen = new Brush(); (this.StarColor this.StarSize);
+
+            if (this.Length > 2 && LineEnd != this.Location)
+            {
+                var linGrBrush = new LinearGradientBrush(
+                   CurrLoc,
+                   LineEnd,
+                   SkyColor,   
+                   this.StarColor
+                );  //*/
+
+                var pen = new Pen(linGrBrush, this.StarSize);
+
+                //e.Graphics.DrawLine(pen, 0, 10, 200, 10);
+                using (var g = Graphics.FromImage(img))
+                    g.DrawLine(pen, CurrLoc.X, CurrLoc.Y, LineEnd.X, LineEnd.Y);
+            }
+            else
+                img.SetPixel(CurrLoc.X, CurrLoc.Y, this.StarColor);
+        }
+        public void Reset()
+        {
+            if (MaxSpawnRadius < 0 || MinSpawnRadius < 0)
+                this.Location = new Point(RandStar.Next(0, ScreenSize.Width - 1), RandStar.Next(0, ScreenSize.Height - 1));
+            else
+            {
+                var angle = RandStar.NextDouble() * 2 * Math.PI;
+                var radius = (MaxSpawnRadius - MinSpawnRadius) * RandStar.NextDouble() + MinSpawnRadius;
+                this.Location = new Point((int)(radius * Math.Cos(angle)) + Center.X, (int)(radius * Math.Sin(angle)) + Center.Y);
+            }
+
+            if (RadialMotion != 0)
+            {
+                this.Location = new Point(this.Location.X - Center.X, this.Location.Y - Center.Y);
+                this.Direction = new PointF(this.Location.X, this.Location.Y);
+            }
+            else
+                this.Direction = new PointF(this.Location.X - Center.X, this.Location.Y - Center.Y);
+
+            var mag = (float)Math.Sqrt(Math.Pow(this.Direction.X, 2) + Math.Pow(this.Direction.Y, 2));
+            this.Direction = new PointF(this.Direction.X / mag, this.Direction.Y / mag);
+
+            this.StarSize = RandStar.Next(0, 4);
+            if (RandomStarColor)
+                this.StarColor = Color.FromArgb(RandStar.Next(0, 255), RandStar.Next(0, 255), RandStar.Next(0, 255));
+            else
+                this.StarColor = Colors[RandStar.Next(0, Colors.Length - 1)];
+
+            this.Length = 1.0f;
+            this.Velocity = RandStar.Next(10, 10);
+        }
+
+        private void StarBoundsCheck()
+        {
+            bool success = false;
+
+            while (!success)
+            {
+                if (RadialMotion == 0 && (this.Location.X < 0 || this.Location.X >= ScreenSize.Width || this.Location.Y < 0 || this.Location.Y >= ScreenSize.Height))
+                    this.Reset();
+                else if (RadialMotion != 0)
+                {
+                    var CurrLoc = new Point(this.Location.X + Center.X, this.Location.Y + Center.Y);
+                    if (CurrLoc.X < 0 || CurrLoc.X >= ScreenSize.Width || CurrLoc.Y < 0 || CurrLoc.Y >= ScreenSize.Height)
+                        this.Reset();
+                    else
+                        success = true;
+                }
+                else
+                    success = true;
+            }
+
+        }
+    }
+}
